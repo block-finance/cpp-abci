be0bb13d926965cdf3fed25349be2ec5ad09fd83
Martin Dyring-Andersen <martin@dyring-andersen.dk>
2016-04-15 23:11:35 -0400

    Fix list of required boost dependencies

--- build.cmd
+++ build.cmd
@@ -11,7 +11,7 @@ if exist "%INSTALL_PREFIX%" (goto done)
 pushd "%BOOST_PREFIX%"
 call bootstrap.bat
 b2 headers
-set BOOST_LIBRARIES="--with-log --with-system --with-thread"
+set BOOST_LIBRARIES=--with-log --with-system --with-thread
 b2 --toolset=msvc-14.0 --prefix="%INSTALL_PREFIX%\debug" --layout=tagged %BOOST_LIBRARIES% address-model=64 link=static runtime-link=static variant=debug install
 b2 --toolset=msvc-14.0 --prefix="%INSTALL_PREFIX%\release" --layout=tagged %BOOST_LIBRARIES% --with-thread address-model=64 link=static runtime-link=static variant=release install
 popd
