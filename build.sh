7daf07154c65a4e3dceaa4f438b24b12d6729dd2
Martin Dyring-Andersen <martin@dyring-andersen.dk>
2016-05-02 09:09:43 -0400

    Detect Boost Build toolset based on $CC

--- build.sh
+++ build.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 JOBS=2
 DEPENDENCIES_PREFIX="$PWD/dependencies"
+INSTALL_PREFIX="$DEPENDENCIES_PREFIX/install"
 BOOST_PREFIX="$DEPENDENCIES_PREFIX/boost"
 PROTOBUF_PREFIX="$DEPENDENCIES_PREFIX/protobuf"
 TMSP_PREFIX="$DEPENDENCIES_PREFIX/tmsp"
-INSTALL_PREFIX="$DEPENDENCIES_PREFIX/install"
+
+if [ -z “$CC” ]
+then
+	TOOLSET=gcc
+else
+	TOOLSET=$CC
+fi
 
 if ! [ -d "$INSTALL_PREFIX" ]
 then
         pushd "$BOOST_PREFIX"
         ./bootstrap.sh --prefix="$INSTALL_PREFIX" --with-libraries=log,system,thread
 		./b2 -d0 headers
-        ./b2 -d0 -j$JOBS cxxflags=-std=c++11 link=static install
+        ./b2 -d0 -j$JOBS toolset=$TOOLSET link=static install
         popd
 
         pushd "$PROTOBUF_PREFIX/cmake"
@@ -25,4 +32,4 @@ then
         popd
 fi
 
-$BOOST_PREFIX/b2 -sBOOST_ROOT="$BOOST_PREFIX" -d0 -j$JOBS variant=release $@
+$BOOST_PREFIX/b2 -sBOOST_ROOT="$BOOST_PREFIX" -j$JOBS toolset=$TOOLSET variant=release $@
