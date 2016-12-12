768c86b6467b0d895268819c14d732bf6f993a30
Martin Dyring-Andersen <martin@dyring-andersen.dk>
2016-12-12 09:14:46 -0500

    Whitespace

--- build.sh
+++ build.sh
@@ -15,21 +15,21 @@ fi
 
 if ! [ -d "$INSTALL_PREFIX" ]
 then
-        pushd "$BOOST_PREFIX"
-        ./bootstrap.sh --prefix="$INSTALL_PREFIX" --with-libraries=log,system,thread
-		./b2 -d0 headers
-        ./b2 -d0 -j$JOBS $TOOLSET cxxflags=-std=c++11 link=static install
-        popd
+	pushd "$BOOST_PREFIX"
+	./bootstrap.sh --prefix="$INSTALL_PREFIX" --with-libraries=log,system,thread
+	./b2 -d0 headers
+	./b2 -d0 -j$JOBS $TOOLSET cxxflags=-std=c++11 link=static install
+	popd
 
-        pushd "$PROTOBUF_PREFIX/cmake"
-        cmake -DCMAKE_INSTALL_PREFIX="$INSTALL_PREFIX" -DCMAKE_BUILD_TYPE=Release -Dprotobuf_BUILD_TESTS=OFF .
-        make -j $JOBS -s install
-        popd
+	pushd "$PROTOBUF_PREFIX/cmake"
+	cmake -DCMAKE_INSTALL_PREFIX="$INSTALL_PREFIX" -DCMAKE_BUILD_TYPE=Release -Dprotobuf_BUILD_TESTS=OFF .
+	make -j $JOBS -s install
+	popd
 
-        mkdir "$INSTALL_PREFIX/include/tmsp"
-        pushd "$TMSP_PREFIX/types"
-        "$INSTALL_PREFIX/bin/protoc" --cpp_out="$INSTALL_PREFIX/include/tmsp" types.proto
-        popd
+	mkdir "$INSTALL_PREFIX/include/tmsp"
+	pushd "$TMSP_PREFIX/types"
+	"$INSTALL_PREFIX/bin/protoc" --cpp_out="$INSTALL_PREFIX/include/tmsp" types.proto
+	popd
 fi
 
 $BOOST_PREFIX/b2 -sBOOST_ROOT="$BOOST_PREFIX" -j$JOBS $TOOLSET cxxflags=-std=c++11 variant=release $@
